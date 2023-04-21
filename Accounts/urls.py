9e04c36e29d735c5aeb18f82a217e32e4d76f035
nabpar <napaofficial7@gmail.com>
2023-04-21 22:20:07 +0545

    hosting

--- Accounts/urls.py
+++ Accounts/urls.py
@@ -1,16 +1,31 @@
 from django.urls import path
 
-from Accounts.views import( 
-    UserRegiatrationView,UserLoginView,UserProfileView
-    ,UserPasswordChangeView,PasswordResetView,UserPasswordResetView)
-
-
+from Accounts.views import (
+    PasswordResetView,
+    UserLoginView,
+    UserPasswordChangeView,
+    UserPasswordResetView,
+    UserProfileView,
+    UserRegiatrationView,
+)
 
 urlpatterns = [
-    path('register/',UserRegiatrationView.as_view(),name='register'),
-    path('login/',UserLoginView.as_view(),name="UserLogin"),
-    path('profile/',UserProfileView.as_view(),name="user_view"),
-    path('changepassword/',UserPasswordChangeView.as_view(),name="user_password_change"),
-    path('send_reset_password_email/',PasswordResetView.as_view(),name="user_password_reset"),
-    path('user_password_reset/<uid>/<token>/',UserPasswordResetView.as_view(),name="user_password_reset"),
+    path(
+        "register/", UserRegiatrationView.as_view(), name="register"
+    ),  # Registation of user
+    path("login/", UserLoginView.as_view(), name="UserLogin"),  # User Login
+    path("profile/", UserProfileView.as_view(), name="user_view"),  # Profile View
+    path(
+        "changepassword/", UserPasswordChangeView.as_view(), name="user_password_change"
+    ),  # Password Change
+    path(
+        "send_reset_password_email/",
+        PasswordResetView.as_view(),
+        name="user_password_reset",
+    ),  # Passowrd Reset
+    path(
+        "user_password_reset/<uid>/<token>/",
+        UserPasswordResetView.as_view(),
+        name="user_password_reset",
+    ),  # PPassword reset token send
 ]
